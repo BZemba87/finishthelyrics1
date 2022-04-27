b71ea4660673f47bc1ca01bce815d5c29d424be9
BZemba87 <zembabernadette@gmail.com>
2022-04-27 22:08:57 +0000

    Add showQuestion. setNextQuestion, resetState

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,59 @@
-const startButton = document.getElementById("start-btn")
-const quizContainer = document.getElementById("quiz-container")
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('questions')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
-console.log('started')
 startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random( ) - .5 )
+currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion();
 }
 
 function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => { 
+        const button = document.createElement('button');
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button);
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
 }
 
 const questions = [
    {
-    lyrics:  "Go easy on me, baby, I was still a  ____",
+    question:  "Go easy on me, baby, I was still a ....",
     answers: [
-        {text: 'CHILD',  correct: true}
+        {text: 'CHILD',  correct: true},
         {text: 'BABY', correct: false}
-    ]
-    correctAnswer: "a"
-} ,
-{
-    lyrics: "And you know me, turn the O-2 into ____",
-    options: {
-        a: "A SQUARE",
-        b: "FIRE",
-        c: "THE O-3",
-    },
-    correctAnswer: "c"
-},
-{
-    lyrics: "And mother always told me, be careful of ____",
-    options: {
-        a: "WHO YOU TRUST",
-        b: "WHO YOU LOVE",
-        c: "WHO YOU HATE",
-    },
-    correctAnswer: "b"
-},
-{
-    lyrics: "I took a sip from my ____ cup",
-    options: {
-        a: "TEA",
-        b: "DEVIL'S",
-        c: "COFFEE",
-    },
-    correctAnswer: "b",
-    },
-    {
-        lyrics: "She's got a ____ that it seems to me",
-        options: {
-            a: "LOOK",
-            b: "FACE",
-            c: "SMILE",
-        },
-        correctAnswer: "c",
-    },
-    {
-        lyrics: "It may be quite ___ , but now that it's done",
-        options: {
-            a: "SIMPLE",
-            b: "DIFFICULT",
-            c: "STUPID",
-        },
-        correctAnswer: "a",
-    }
-] ;
+    ] 
+   },
+]
