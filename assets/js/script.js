5474ae91610fb26fc65ccc0c8de1cb18723f4e2b
BZemba87 <zembabernadette@gmail.com>
2022-04-30 11:14:13 +0000

    Add styling to body and question area

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,35 @@ let lyrics = {
   correctAnswer: 0
 };
 
+//*funtion to show the lyric questions, alternatives and correct answer
 function showLyrics(l) {
    //* selecting DOM element
    let questionDiv = document.getElementById("question");
-   //* display the lyric question
+   //* modify the lyric question
    questionDiv.textContent = l.question;
   //*selecting alternatives by a query
   let alts = document.querySelectorAll('.alternative');
-
+  //* loop through alternative answers 
   alts.forEach(function(element, index) {
   element.textContent = l.alternatives[index];
+
+  //*listen for alternative that user clicks
+  element.addEventListener('click', function(){
+    // check correct answer
+if(l.correctAnswer == index) {
+    console.log('Correct Answer!')
+}
+else {
+    console.log('Wrong Answer!')
+}
 });
+ })
 }
 
 showLyrics(lyrics)
 
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+  console.log('Clicked!')
+})
 
