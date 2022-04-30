c98eecea161afb529cb8e1eb2589c02371ff1c35
BZemba87 <zembabernadette@gmail.com>
2022-04-30 15:20:32 +0000

    Add hover on next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,19 @@
 let lyrics = {
   question: 'Go easy on me, baby, I was still a ....',
   alternatives: ['CHILD', 'GIRL', 'MAN', 'WOMAN'],
-  correctAnswer: 0
+  correctAnswer: 0,
+  question: "And you know me, turn the O-2 into ...",
+  alternatives: ['A SQUARE', 'A FIRE', 'THE O-3', 'APARTMENTS'],
+  correctAnswer: 2,
+  question: "And mother always told me, be careful of who you...",
+  alternatives: ['TRUST', 'LOVE', 'HATE', 'BECOME'],
+  correctAnswer: 1,
+  question: "I took a sip from my ... cup",
+  alternatives: ['TEA', 'DEVILS', 'COFFEE', 'SIPPY'],
+  correctAnswer: 1,
+  question: "She's got a ... that it seems to me",
+  alternatives: ['LOOK', 'FACE', 'SMILE', 'STARE'],
+  correctAnswer: 2,
 };
 
 
@@ -23,9 +35,11 @@ function showLyrics(l) {
     // check correct answer
 if(l.correctAnswer == index) {
     alert('Yas Queen!');
+    incrementScore();
 }
 else {
     alert('Oh No!');
+    incrementWrongAnswer();
 }
 });
  })
@@ -33,16 +47,20 @@ else {
 /*runs showLyrics function*/
 showLyrics(lyrics)
 
-
+/*listen for user clicks on next button*/
 let btn = document.getElementById('btn');
 btn.addEventListener('click', function(){
 })
 
+/*gets current score and increments it by 1*/
 function incrementScore() {
-
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
 }
 
-function incrementWrongAnswer() {
-
+/*gets wrong answer score and increments it by 1*/
+function incrementWrongAnswer(){
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
 }
 
