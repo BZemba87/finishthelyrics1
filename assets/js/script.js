7c8aa45156fe13a90a8c2f0bb9a762cffb53d07d
BZemba87 <zembabernadette@gmail.com>
2022-05-02 20:57:32 +0000

    Change structure of q and a section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,42 @@
 //* lyric questions and answers
-let lyrics = {
+let lyrics = [
+  {
   question: 'Go easy on me, baby, I was still a ....',
-  alternatives: ['CHILD', 'GIRL', 'MAN', 'WOMAN'],
+  alternative: [ "CHILD", 
+                         "MAN", 
+                         "WOMAN",
+                         "GIRL"
+  ],
   correctAnswer: 0,
+},
+{
   question: "And you know me, turn the O-2 into ...",
-  alternatives: ['A SQUARE', 'A FIRE', 'THE O-3', 'APARTMENTS'],
+  alternative: ["A SQUARE",
+                      "FIRE",
+                      "THE 0-3",
+                      "APARTMENTS",
+],
   correctAnswer: 2,
-  question: "And mother always told me, be careful of who you...",
-  alternatives: ['TRUST', 'LOVE', 'HATE', 'BECOME'],
-  correctAnswer: 1,
-  question: "I took a sip from my ... cup",
-  alternatives: ['TEA', 'DEVILS', 'COFFEE', 'SIPPY'],
-  correctAnswer: 1,
-  question: "She's got a ... that it seems to me",
-  alternatives: ['LOOK', 'FACE', 'SMILE', 'STARE'],
-  correctAnswer: 2,
-};
-
+  },
+];
 
 //*funtion to show the lyric questions, alternatives and correct answer
-function showLyrics(l) {
+function showLyrics() {
    //* selecting DOM element
    let questionDiv = document.getElementById("question");
    //* modify the lyric question
-   questionDiv.textContent = l.question;
+   questionDiv.textContent = lyrics[0].question;
   //*selecting alternatives by a query
   let alts = document.querySelectorAll('.alternative');
+
   //* loop through alternative answers 
   alts.forEach(function(element, index) {
-  element.textContent = l.alternatives[index];
+  element.textContent = lyrics[0].alternative[index];
 
   //*listen for alternative that user clicks
   element.addEventListener('click', function(){
     // check correct answer
-if(l.correctAnswer == index) {
+if(lyrics[0].correctAnswer == index) {
     alert('Yas Queen!');
     incrementScore();
 }
@@ -45,11 +48,11 @@ else {
  });
 }
 /*runs showLyrics function*/
-showLyrics(lyrics);
+showLyrics();
 
 /*listen for user clicks on next button*/
-let btn = document.getElementById('btn');
-btn.addEventListener('click', showLyrics());
+let btn = document.getElementById("btn");
+btn.addEventListener("click", showLyrics);
 
 /*gets current score and increments it by 1*/
 function incrementScore() {
